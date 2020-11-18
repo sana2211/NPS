8ed20baafea29d0f5f204cd00166ccd3d6771541
sana2211 <70413361+sana2211@users.noreply.github.com>
2020-11-18 15:42:37 -0600

    Update script.js

--- nps3-3 (1)/script.js
+++ nps3-3 (1)/script.js
@@ -1,8 +1,6 @@
 'use strict';
 // put your own value below!
-const apiKey = "xK8CdTImrEHLFRftUhG6MlpPHiLeQEd7R9hnxCt1"
-;
-
+const apiKey = "xK8CdTImrEHLFRftUhG6MlpPHiLeQEd7R9hnxCt1";
 const searchURL = "https://developer.nps.gov/api/v1/parks";
 
 function submitForm(){
@@ -15,7 +13,8 @@ function submitForm(){
 }
 
 function formatQueryParams(params){
-  const queryItems = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  const queryItems = Object.keys(params)
+  .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
   return queryItems.join('&');
 }
 
@@ -25,17 +24,25 @@ function getParkResults(query, limit){
       stateCode: query,      
       limit: limit
   };
-  const queryString = formatQueryParams(params)
-  const url = searchURL + '?' + queryString;
+let queryString = $.param(params);
+ const url = searchURL + '?' + queryString;
+     console.log("url", url);
+    
   fetch(url)
-    .then(response =>response.json())
-    .then(response=>renderParkResults(response.data))
-    .catch(err =>alert("No Park found!"));    
+         if(response.ok){
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    }).then(responseJson => displayResults(responseJson.data))
+    .catch(err=> {
+        $("#js-error-message").text(`Something failed: ${err.message}`);
+    })
 }
 
-function renderParkResults(parkList){
+function displayResults(parkList){
+  console.log(parkList)
     $('#results').html("");
-    $('#results').text("Check your search result below:");
+    $('.result-header').append("Check your search result below:");
     parkList.forEach(item =>{
     $('#results').append(`<li><h3>${item.fullName}</h3>
     <p>${item.description}</p><a href=${item.url}>Park's Website</a><div id="addresses"><h5>${item.addresses[0].type}:</h5><p class="addresses">${item.addresses[0].line1}</p><p class="addresses">${item.addresses[0].city}</p><p class="addresses">${item.addresses[0].stateCode}</p>
