e6faa62f5da1e091d3e44d5fac3294f81aff0a3a
sana2211 <70413361+sana2211@users.noreply.github.com>
2020-11-19 14:34:02 -0600

    Update script.js

--- nps3-3 (1)/script.js
+++ nps3-3 (1)/script.js
@@ -4,54 +4,50 @@ const apiKey = "xK8CdTImrEHLFRftUhG6MlpPHiLeQEd7R9hnxCt1";
 const searchURL = "https://developer.nps.gov/api/v1/parks";
 
 function submitForm(){
-    $('.js-form').submit(e => {
-      e.preventDefault();
-      const userInput = $('#stateInput').val();
-      const numResults = $('#resultInput').val();
-      getParkResults(userInput, numResults);
-    });
+  $('#parkForm').submit(event => {
+    event.preventDefault();
+    if(!/^\w(\s*,?\s*\w)*$/.test($("#stateInput").val())){
+      alert("Invalid input");
+      return;
+    }
+    const userInput = $('#stateInput').val().split(",");
+    const numResults = $('#resultInput').val();
+    getParkResults(userInput, numResults);
+  });
 }
-
+//Format Search Query
 function formatQueryParams(params){
-  const queryItems = Object.keys(params)
-  .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  const queryItems = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
   return queryItems.join('&');
 }
-
-function getParkResults(query, limit){
+//GET Request to NPS API
+function getParkResults(query, maxResults){
   const params = {
-      api_key: apiKey,
-      stateCode: query,      
-      limit: limit
+    api_key: apiKey,
+    stateCode: query,      
+    limit: maxResults
   };
-let queryString = $.param(params);
- const url = searchURL + '?' + queryString;
-     console.log("url", url);
-    
+  const queryString = formatQueryParams(params);
+  const url = searchURL + '?' + queryString;  
+  console.log(url);  
   fetch(url)
-         if(response.ok){
-            return response.json();
-        }
-        throw new Error(response.statusText);
-    }).then(responseJson => displayResults(responseJson.data))
-    .catch(err=> {
-        $("#js-error-message").text(`Something failed: ${err.message}`);
-    })
+    .then(response =>response.json())
+    .then(response => renderParkResults(response.data))
+    .catch(err =>alert(err));    
+}
+//Render the GET Request results to the DOM
+function renderParkResults(parkList){    
+  console.log(parkList)  
+  $('#results-list').empty();
+  $('#results').text("Check your search result below:");
+  parkList.forEach(item =>{
+  $('#results').append(`<li><h3>${item.fullName}</h3>
+  <p>${item.description}</p><a href=${item.url}>Park's Website</a><div id="addresses"><h5>${item.addresses[0].type}:</h5><p class="addresses">${item.addresses[0].line1}</p><p class="addresses">${item.addresses[0].line2}</p><p class="addresses">${item.addresses[0].line3}</p>
+  <p class="addresses">${item.addresses[0].city}, ${item.addresses[0].stateCode}, ${item.addresses[0].postalCode}</p></div>
+  <p class="states"><b>${item.states}</b></p></div>
+  </li>`)
+});
 }
 
-function displayResults(parkList){
-  console.log(parkList)
-    $('#results').html("");
-    $('.result-header').append("Check your search result below:");
-    parkList.forEach(item =>{
-    $('#results').append(`<li><h3>${item.fullName}</h3>
-    <p>${item.description}</p><a href=${item.url}>Park's Website</a><div id="addresses"><h5>${item.addresses[0].type}:</h5><p class="addresses">${item.addresses[0].line1}</p><p class="addresses">${item.addresses[0].city}</p><p class="addresses">${item.addresses[0].stateCode}</p>
-    <p class="addresses">${item.addresses[0].city}, ${item.addresses[0].stateCode}, ${item.addresses[0].postalCode}</p></div></li>`)
-    });
-}
-
-function init(){
-    submitForm();
-}
-
-$(init);
+$(submitForm);
+    
